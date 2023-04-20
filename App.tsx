72fff3ac03079b21642dd1b2d555cb42372477d4
Patato <pathonbusiness@gmail.com>
2023-04-20 21:40:16 +0700

    global styles and font

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import Home from './screens/Home';
+import AppLoading from 'expo-app-loading';
+
+const getFonts = () => Font.loadAsync({ 
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'), 
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+}); 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false)
+  
+  if(fontsLoaded){
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading 
+        // startAsync={getFonts}
+        // onFinish={()=> setFontsLoaded(true)} 
+        // onError={console.warn}
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      /> 
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
