2337cd72f21e032124b645b1054f69e12a895fa8
Patato <pathonbusiness@gmail.com>
2023-04-22 10:05:51 +0700

    fix about stack

--- routes/AboutStack.tsx
+++ routes/AboutStack.tsx
@@ -1,10 +1,26 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { NavigationContainer } from "@react-navigation/native";
-import About from "../screens/About";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import About from "../screens/About";
+import Header from "../shared/Header";
+
+const Stack = createNativeStackNavigator()
 
 export default function AboutStack() {
 	return (
-			<About />
+		<Stack.Navigator
+      screenOptions={{
+          headerTitle: () => <Header />,
+					headerStyle: {
+						backgroundColor: "#eee",
+					},
+					headerTintColor: "#444",
+        }}
+      >
+        
+        <Stack.Screen
+          name="About"
+          component={About}
+        />
+      </Stack.Navigator>
 	)
 }
--- routes/HomeStack.tsx
+++ routes/HomeStack.tsx
@@ -3,13 +3,15 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "../screens/Home";
 import ReviewDetails from "../screens/ReviewDetails";
+import Header from "../shared/Header";
 
 const Stack = createNativeStackNavigator();
 
 export default function HomeStack() {
   return (
       <Stack.Navigator
-        screenOptions={{
+      screenOptions={{
+          headerTitle: () => <Header />,
 					headerStyle: {
 						backgroundColor: "#eee",
 					},
